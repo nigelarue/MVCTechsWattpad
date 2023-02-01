fe45f20fc25c7627e9727e0f736f90234ddc63d7
nigelarue <larue.nigel91@gmail.com>
2023-02-01 16:41:19 -0600

    created layouts in handlebars template files, inputted javascript logic and POST routes for those files in public js files to link for each template and input data in document.

---
+++ public/js/signup.js
@@ -0,0 +1,23 @@
+const signupForm = async function(event) {
+    event.preventDefault();
+
+    const usernameEl = document.querySelector('#username-input-signup');
+    const passwordEl = document.querySelector('#password-input-signup');
+
+    const response = await fetch('/api/user', {
+        method: 'POST',
+        body: JSON.stringify({
+            username: usernameEl.value,
+            password: passwordEl.value,
+        }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert('ERROR: unable to sign up');
+    }
+};
+
+document.querySelector('#signup-form').addEventListener('submit', signupForm);
