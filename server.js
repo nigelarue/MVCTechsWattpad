5aaf185599f56f5f429252ef70f1296e2678a34f
nigelarue <larue.nigel91@gmail.com>
2023-01-31 19:07:12 -0600

    updated server.js to properly source handlebars, storage and port location.

--- server.js
+++ server.js
@@ -3,20 +3,15 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
-const sequelize = require('./config/config');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+const sequelize = require('./config/config');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-app.engine('handlebars', exphbs.create({ helpers }).engine);
-app.set('view engine', 'handlebars');
-
-app.use(session({
+const sessStore = {
   secret: 'Super secret secret',
   cookie: {
     maxAge: 300000,
@@ -27,9 +22,20 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize,
-  }),
-}));
+    db: sequelize
+  })
+};
+
+app.use(session(sessStore));
+
+const handleBs = exphbs.create({ helpers });
+
+app.engine('handlebars', handleBs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
 
