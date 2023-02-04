a5c2e789660446a8ab2488b955cf391071221981
nigelarue <larue.nigel91@gmail.com>
2023-02-03 18:28:29 -0600

    github issue with previous pull once more, had to rearrange CSS and restructure server.js

--- server.js
+++ server.js
@@ -5,15 +5,13 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers/');
 const helpers = require('./utils/helpers');
 
-
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/config');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const sessStore = {
+const sess = {
   secret: 'Super secret secret',
   cookie: {
     maxAge: 300000,
@@ -28,19 +26,24 @@ const sessStore = {
   })
 };
 
-app.use(session(sessStore));
+app.use(session(sess));
 
+// Set up Handlebars.js engine with custom helpers
 const handleBs = exphbs.create({ helpers });
 
+// Inform Express.js on which template engine to use
 app.engine('handlebars', handleBs.engine);
 app.set('view engine', 'handlebars');
 
+// Express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Turn on routes
 app.use(routes);
 
+// Turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
